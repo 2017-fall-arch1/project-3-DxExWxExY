06d86214935600e201f567c79daafb5559f19bc1
DxEXWxEXY <dxexwxexy@gmail.com>
2017-12-01 14:44:03 -0700

    Cleaned Up and Added Assembly

---
+++ project/pong/move.s
@@ -0,0 +1,66 @@
+	.arch msp430g2553
+	.p2align 1,0
+	.text
+
+	.data
+	.global BIT0
+BIT0:
+	.byte 0x1
+
+	.data
+	.global BIT1
+BIT1:
+	.byte 0x2
+
+	.data
+	.global BIT2
+BIT2:
+	.byte 0x4
+
+	.data
+	.global BIT3
+BIT3:
+	.byte 0x8
+
+	
+	.text
+	.global move
+move:
+	cmp.b &P2IN, &BIT0	;Checs is SW1 was pressed
+	jnz SW2
+	mov #pLU, r12		;parameters for movlayerdraw
+	mov #pl, r13
+	call #movLayerDraw
+	mov #fieldFence, r13	;parameters for mladvance
+	call mlAdvance
+	ret
+	
+SW2:
+	cmp.b &P2IN, &BIT1	;Checs is SW2 was pressed
+	jnz SW3
+	mov #pLD, r12		;parameters for movlayerdraw
+	mov #pl, r13
+	call #movLayerDraw
+	mov #fieldFence, r13	;parameters for mladvance
+	call mlAdvance
+	ret
+
+SW3:	cmp.b &P2IN, &BIT0	;Checs is SW3 was pressed
+	jnz SW4
+	mov #pRD, r12		;parameters for movlayerdraw
+	mov #pl, r13
+	call #movLayerDraw
+	mov #fieldFence, r13	;parameters for mladvance
+	call mlAdvance
+	ret
+
+SW4:
+	cmp.b &P2IN, &BIT0	;Checs is SW4 was pressed
+	jnz END
+	mov #pLU, r12		;parameters for movlayerdraw
+	mov #pl, r13
+	call #movLayerDraw
+	mov #fieldFence, r13	;parameters for mladvance
+	call mlAdvance
+END:
+	ret
---
+++ project/pong/pong.h
@@ -0,0 +1,19 @@
+#ifndef pong_included
+#define pong_included
+
+#define GREEN_LED BIT6
+
+typedef struct MovLayer_s {
+    Layer *layer;
+    Vec2 velocity;
+    struct MovLayer_s *next;
+} MovLayer;
+
+void movLayerDraw(MovLayer *movLayers, Layer *layers);
+void mlAdvance(MovLayer *ml, Region *fence);
+void paddleAdvance(MovLayer *ml, MovLayer *pLU, MovLayer *pRU, Region *fence);
+void startup();
+void score();
+void moveC();
+
+#endif
